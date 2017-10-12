02bd1b1d4d02c82595e01ef48bb19d6182c116fc
Yinuo Zhu <enorno18@gmail.com>
2017-10-11 22:49:44 -0700

    first commit

---
+++ src/whaletalk.js
@@ -0,0 +1,21 @@
+let input = 'Turpentine and turtles';
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+
+for (inputIndex=0; inputIndex<input.length; inputIndex++) {
+  
+  for (vowelsIndex=0; vowelsIndex<vowels.length; vowelsIndex++) {
+    if (input[inputIndex]===vowels[vowelsIndex]) {
+     
+     resultArray.push(input[inputIndex]);    
+    }
+  }
+  if (input[inputIndex]===vowels[1]) {
+    resultArray.push(input[inputIndex]);
+  }else if (input[inputIndex]===vowels[4]) {
+    resultArray.push(input[inputIndex]);
+  }
+}
+
+console.log(resultArray.join('').toUpperCase());
